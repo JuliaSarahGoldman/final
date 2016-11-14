d35a4702660921dd1743b968e2ab9dabb0cbdb6c
jdr3 <jdr3@61224a88-a156-e611-bf22-b8ac6f268050>
2016-11-14 03:38:10 +0000

    Sphere and Bumps and Welding

--- source/Planet.cpp
+++ source/Planet.cpp
@@ -1,8 +1,7 @@
 #include "Planet.h"
-#include <unordered_map>
 
 void Planet::writeSphere(String filename, shared_ptr<Array<Vector3>>& vertices, shared_ptr<Array<Vector3int32>>& faces) {
-    makeIcohedron(1.0f, vertices, faces);
+    makeIcohedron(5.0f, vertices, faces);
 
     int numVert = vertices->size();
     int numFace = faces->size();
@@ -27,21 +26,20 @@ void Planet::writeSphere(String filename, shared_ptr<Array<Vector3>>& vertices, 
 }
 
 void Planet::makeIcohedron(float radius, shared_ptr<Array<Vector3>>& vertices, shared_ptr<Array<Vector3int32>>& faces) {
-    float t = (1.0 + sqrt(5.0)) / 2.0;
-    float s = sqrt(1 + square(t));
+    float t = (1.0f + sqrt(5.0f)) / 2.0f;
 
-    vertices->append(radius * Vector3(t, 1, 0) / s);
-    vertices->append(radius * Vector3(-t, 1, 0) / s);
-    vertices->append(radius * Vector3(t, -1, 0) / s);
-    vertices->append(radius * Vector3(-t, -1, 0) / s);
-    vertices->append(radius * Vector3(1, 0, t) / s);
-    vertices->append(radius * Vector3(1, 0, -t) / s);
-    vertices->append(radius * Vector3(-1, 0, t) / s);
-    vertices->append(radius * Vector3(-1, 0, -t) / s);
-    vertices->append(radius * Vector3(0, t, 1) / s);
-    vertices->append(radius * Vector3(0, -t, 1) / s);
-    vertices->append(radius * Vector3(0, t, -1) / s);
-    vertices->append(radius * Vector3(0, -t, -1) / s);
+    vertices->append(radius * Vector3(t, 1, 0));
+    vertices->append(radius * Vector3(-t, 1, 0));
+    vertices->append(radius * Vector3(t, -1, 0));
+    vertices->append(radius * Vector3(-t, -1, 0));
+    vertices->append(radius * Vector3(1, 0, t));
+    vertices->append(radius * Vector3(1, 0, -t));
+    vertices->append(radius * Vector3(-1, 0, t));
+    vertices->append(radius * Vector3(-1, 0, -t));
+    vertices->append(radius * Vector3(0, t, 1));
+    vertices->append(radius * Vector3(0, -t, 1));
+    vertices->append(radius * Vector3(0, t, -1));
+    vertices->append(radius * Vector3(0, -t, -1));
 
     faces->append(Vector3int32(0, 8, 4));
     faces->append(Vector3int32(1, 10, 7));
@@ -64,20 +62,47 @@ void Planet::makeIcohedron(float radius, shared_ptr<Array<Vector3>>& vertices, s
     faces->append(Vector3int32(6, 1, 3));
     faces->append(Vector3int32(11, 7, 5));
 
-    for(int i(0); i < 6; ++i){
+    for(int i(0); i < 8; ++i){
         subdivideIcoHedron(radius, vertices, faces);
     }
+    
+    //const G3D::Welder::Settings settings;
+    Array<Vector3> normals;
+    Array<Vector2> texture;
+    Array<int> indices;
+    Array<Vector3> verts;
+    
+    for (int i(0); i < vertices->length(); ++i) {
+        verts.append(vertices->operator[](i));
+    }
+
+    for(int i(0); i < faces->length(); ++i){
+        Vector3int32 face = faces->operator[](i);
+        indices.append(face.x, face.y, face.z);
+    }
+
+    Welder::weld(verts, texture, normals, indices, G3D::Welder::Settings());
+
+    faces = std::make_shared<Array<Vector3int32>>();
+    for(int i(0); i < indices.size(); i += 3) {
+        faces->append(Vector3int32(indices[i], indices[i+1], indices[i+2]));
+    }
+
+    for(int i(0); i < verts.size(); ++i) {
+        verts[i] += normals[i] * Random::threadCommon().uniform(1.0, 1.2);
+    }
+    vertices = std::make_shared<Array<Vector3>>(verts);
 }
 
 void Planet::getMiddle(float radius, Vector3& v1, Vector3& v2, Vector3& newVector) {
-    float t = (1 + sqrt(5.0f)) / 2;
-    newVector = (v2 - v1)*0.5 + v1;
-    newVector.unit();
-    newVector *= (sqrt(t * t + 1) * radius) / t;
+    float t = (1.0f + sqrt(5.0f)) / 2.0f;
+    newVector = (v2 + v1)/2.0f;
+    newVector = newVector.unit();
+    newVector *= sqrt(t*t+1)*radius;
 }
 
 void Planet::subdivideIcoHedron(float radius, shared_ptr<Array<Vector3>>& vertices, shared_ptr<Array<Vector3int32>>& faces) {
-    float t = (1 + sqrt(5)) / 2;
+    float t = (1.0f + sqrt(5.0f)) / 2.0f;
 
     Vector3 newVec1;
     Vector3 newVec2;
@@ -87,7 +112,7 @@ void Planet::subdivideIcoHedron(float radius, shared_ptr<Array<Vector3>>& vertic
     
     shared_ptr<Array<Vector3int32>> newFaces = std::make_shared<Array<Vector3int32>>();
 
-    for (int i(0); i < numFaces; i += 3) {
+    for (int i(0); i < numFaces; ++i) {
     
         int numVertices = vertices->length();
         Vector3int32 face = faces->operator[](i);
@@ -103,12 +128,39 @@ void Planet::subdivideIcoHedron(float radius, shared_ptr<Array<Vector3>>& vertic
 
         vertices->append(newVec1, newVec2, newVec3);
 
+        int posVec1 = numVertices;
+        int posVec2 = numVertices + 1;
+        int posVec3 = numVertices + 2;
+
         //add the new faces
-        Vector3 temp;
-        newFaces->append(Vector3int32(numVertices + 2, face.x, numVertices    ));
-        newFaces->append(Vector3int32(numVertices    , face.y, numVertices + 1));
-        newFaces->append(Vector3int32(numVertices + 1, face.z, numVertices + 2));
-        newFaces->append(Vector3int32(numVertices, numVertices + 1, numVertices + 2));
+        newFaces->append(Vector3int32(posVec3, face.x, posVec1));
+        newFaces->append(Vector3int32(posVec1, face.y, posVec2));
+        newFaces->append(Vector3int32(posVec2, face.z, posVec3));
+        newFaces->append(Vector3int32(posVec1, posVec2, posVec3));
     }
     faces = newFaces;
 }
+
+/*
+    
+    Table<Vector3, int> vertexPositions;
+    for(int i(0); i < vertices->length(); ++i) {
+        vertexPositions.set(vertices->operator[](i), i);
+    }
+
+            if(!vertexPositions.get(newVec1, posVec1)) {
+            vertices->append(newVec1);
+            vertexPositions.set(newVec1, numVertices);
+            posVec1 = numVertices;
+        }
+        if(!vertexPositions.get(newVec2, posVec2)) {
+            vertices->append(newVec2);
+            vertexPositions.set(newVec2, numVertices + 1);
+            posVec2 = numVertices + 1;
+        }
+        if(!vertexPositions.get(newVec3, posVec3)) {
+            vertices->append(newVec3);
+            vertexPositions.set(newVec3, numVertices + 2);
+            posVec3 = numVertices + 2;
+        }
+*/
